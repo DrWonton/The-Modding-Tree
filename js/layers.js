8789885fabb6101d8f5ca722ea20d1b0d80ddfe7
John G <mynamesjohnglass@gmail.com>
2021-08-17 23:29:09 -0400

    New upgrades

--- js/layers.js
+++ js/layers.js
@@ -1,6 +1,6 @@
 addLayer("p", {
     name: "prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "D", // This appears on the layer's node. Default is the id with the first letter capitalized
+    symbol: "W", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
@@ -8,8 +8,8 @@ addLayer("p", {
     }},
     color: "Green",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "dollars", // Name of prestige currency
-    baseResource: "points", // Name of resource prestige is based on
+    resource: "workers", // Name of prestige currency
+    baseResource: "pizzas", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
@@ -29,22 +29,45 @@ addLayer("p", {
     upgrades: {
         11: {
             name: "Upgrade 1",
-            description: "Bake twice as many pizzas",
+            title: "Oven",
+            description: "Bake twice as many pizzas per second",
             cost: new Decimal(1),
         },
         12: {
             name: "Upgrade 2",
-            description: "Further increase your pizza baking",
-            cost: new Decimal(2),
+            title: "Extra cheese",
+            description: "The most basic of toppings.  Workers boost pizza production",
+            cost: new Decimal(5),
             effect() {
-                return player[this.layer].points.add(1).pow(0.5)
+                return player[this.layer].points.add(1).pow(0.75)
             },
             effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, //add formatting to the effect
         },
         13: {
             name: "Upgrade 3",
-            description: "Further increase your pizza baking",
-            cost: new Decimal(5),
+            title: "Newspaper Ad",
+            description: "Word is spreading fast! Pizza slightly boosts worker gain",
+            cost: new Decimal(10),
+            effect() {
+                return player.points.add(1).pow(0.25)
+            },
+            effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, //add formatting to the effect
+        },
+        14: {
+            name: "Upgrade 4",
+            title: "Sausage",
+            description: "Ovens are twice as effective",
+            cost: new Decimal(25),
+            effect() {
+                return player.points.add(1).pow(0.15)
+            },
+            effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, //add formatting to the effect
+        },
+        15: {
+            name: "Upgrade 5",
+            title: "Meat Lovers",
+            description: "Double the output of ovens once more",
+            cost: new Decimal(100),
             effect() {
                 return player.points.add(1).pow(0.15)
             },
--- js/mod.js
+++ js/mod.js
@@ -13,7 +13,7 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.0",
+	num: "0.1.0",
 	name: "Literally nothing",
 }
 
@@ -45,6 +45,8 @@ function getPointGen() {
 	let gain = new Decimal(1)
 	if(hasUpgrade('p',11)) gain = gain.times(2)
 	if(hasUpgrade('p',12)) gain = gain.times(upgradeEffect('p',12))
+	if(hasUpgrade('p',14)) gain = gain.times(2)
+	if(hasUpgrade('p',15)) gain = gain.times(2)
 	return gain
 }
 
